636c1641abf6c07d636609e9260b2d2223baef87
Subiksha2309 <131661482+Subiksha2309@users.noreply.github.com>
2024-06-26 11:52:05 +0530

    Add files via upload

---
+++ Find factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+    int i=1 ,n , fact=1;
+
+    printf("enter the value of n : ");
+    scanf("%d" ,&n);
+    
+    while ( i <=n)
+    {
+        fact=fact*i;
+        i++; 
+    }
+        printf("factorial of %d" , fact);
+}
+
